5735676452281f8e18c2ca37fc688db429d2e975
vickyadri29 <vickyadri290501@gmail.com>
2023-07-28 10:05:07 +0800

    fix(position): fix position shape title

--- src/components/About/BurstBload.tsx
+++ src/components/About/BurstBload.tsx
@@ -9,7 +9,7 @@ const BurstBload: React.FC = () => {
         animate={{ scale: [0.8, 0.8, 1.05], rotate: [0, 360] }}
         transition={{ ease: 'easeInOut', repeatType: "reverse", repeat: Infinity, duration: 1.2 }}
         alt="about shape vickyadrii"
-        className="absolute -z-10 top-0 md:right-4 right-5 md:w-auto w-6"
+        className="absolute -z-10 top-0 right-4 md:w-auto w-6"
       />
     </div>
   );
