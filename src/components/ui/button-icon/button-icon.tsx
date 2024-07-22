d1139581a440aed4ee5987f0c875ad25f1ad3059
vickyadri29 <vickyadri290501@gmail.com>
2024-07-22 15:53:26 +0700

    style: adjust the styling of the hero section

--- src/components/ui/button-icon/button-icon.tsx
+++ src/components/ui/button-icon/button-icon.tsx
@@ -7,9 +7,9 @@ type Props = {
 
 const ButtonIcon = ({ children, icon }: Props & React.ButtonHTMLAttributes<HTMLButtonElement>) => {
   return (
-    <button className="md:px-4 px-3 md:py-2 py-1.5 md:text-base text-sm bg-primary-dark-700 hover:bg-black active:bg-primary-dark-700 border border-primary-dark-600 rounded-md flex items-center md:gap-2 gap-1 transition-all duration-200 ease-out w-fit">
-      <p>{children}</p>
-      {icon && <Image src={icon ?? ""} alt={children} width={24} height={24} />}
+    <button className="md:px-4 px-3 md:py-2 py-1.5 bg-primary-dark-700 hover:bg-black active:bg-primary-dark-700 border border-primary-dark-600 rounded-md flex items-center md:gap-2 gap-1 transition-all duration-200 ease-out w-fit">
+      <p className="md:text-base text-xs">{children}</p>
+      {icon && <Image src={icon ?? ""} alt={children} width={24} height={24} className="md:w-6 w-5 md:h-6 h-5" />}
     </button>
   );
 };
--- src/features/home/components/hero/social-media.tsx
+++ src/features/home/components/hero/social-media.tsx
@@ -6,18 +6,18 @@ import Link from "next/link";
 
 const SocialMedia = () => {
   return (
-    <div className="flex md:flex-row flex-col md:items-center gap-6">
+    <div className="flex items-center md:gap-6 gap-4">
       <Link
         href="https://docs.google.com/document/d/133M4LP55oJaltng-ZbwIoO4Rb8RmRt8CvKZOl12SAFo/edit?usp=sharing"
         target="_blank"
       >
         <ButtonIcon icon={getAssetUrl("/icons/ic_arrow_down.svg")}>View Resume</ButtonIcon>
       </Link>
-      <ul className="flex items-center gap-4">
+      <ul className="flex items-center md:gap-4 gap-4">
         {socialMedia.map(({ href, icon }) => (
           <Link key={href} href={href}>
             <li>
-              <Image src={getAssetUrl(icon)} alt={href} width={24} height={24} />
+              <Image src={getAssetUrl(icon)} alt={href} width={24} height={24} className="md:w-6 w-5 md:h-6 h-5" />
             </li>
           </Link>
         ))}
