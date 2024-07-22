7f5f35250b59a7f53016ab3eab99b10aeec5b44e
vickyadri29 <vickyadri290501@gmail.com>
2024-07-22 11:40:09 +0700

    fix: fix the link

--- src/features/home/components/social-media.tsx
+++ src/features/home/components/social-media.tsx
@@ -1,27 +1,18 @@
-"use client";
-
 import ButtonIcon from "@/components/ui/button-icon/button-icon";
 import { socialMedia } from "@/constants/constants";
 import { getAssetUrl } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 const SocialMedia = () => {
-  const router = useRouter();
   return (
     <div className="flex md:flex-row flex-col md:items-center gap-6">
-      <ButtonIcon
-        onClick={() =>
-          router.push(
-            "https://docs.google.com/document/d/133M4LP55oJaltng-ZbwIoO4Rb8RmRt8CvKZOl12SAFo/edit?usp=sharing"
-          )
-        }
-        icon={getAssetUrl("/icons/ic_arrow_down.svg")}
+      <Link
+        href="https://docs.google.com/document/d/133M4LP55oJaltng-ZbwIoO4Rb8RmRt8CvKZOl12SAFo/edit?usp=sharing"
+        target="_blank"
       >
-        View Resume
-      </ButtonIcon>
-
+        <ButtonIcon icon={getAssetUrl("/icons/ic_arrow_down.svg")}>View Resume</ButtonIcon>
+      </Link>
       <ul className="flex items-center gap-4">
         {socialMedia.map(({ href, icon }) => (
           <Link key={href} href={href}>
