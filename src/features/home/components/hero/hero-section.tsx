c48aa7c436296328f563b1b6a4427831e50e8b47
vickyadri29 <vickyadri290501@gmail.com>
2024-07-27 01:14:34 +0700

    adjust gradient

--- src/features/home/components/hero/hero-section.tsx
+++ src/features/home/components/hero/hero-section.tsx
@@ -4,9 +4,7 @@ const HeroSection = () => {
   return (
     <section className="space-y-4 md:my-32 my-14">
       <h2 className="md:text-[32px] text-2xl text-primary-blue-500 font-caveat font-light">Hi, I&apos;m Vicky!</h2>
-      <h1 className="text-transparent !bg-clip-text bg-gradient-to-r from-primary-dark-400 to-white md:text-5xl text-3xl font-bold">
-        Frontend Engineer
-      </h1>
+      <h1 className="gradient-text md:text-5xl text-3xl md:h-14 inline-block font-bold">Frontend Engineer</h1>
       <p className="text-primary-dark-500 md:text-base text-sm md:leading-8 leading-6">
         Frontend Engineer with 2+ years of experience in React.js for developing a website and web applications.
         Currently exploring about backend development.
