65362da4418e440582b0c40ea02fafb945542114
vickyadri29 <vickyadri290501@gmail.com>
2024-07-27 00:44:43 +0700

    fix: fix gradient on hero section

--- src/features/home/components/hero/hero-section.tsx
+++ src/features/home/components/hero/hero-section.tsx
@@ -4,7 +4,7 @@ const HeroSection = () => {
   return (
     <section className="space-y-4 md:my-32 my-14">
       <h2 className="md:text-[32px] text-2xl text-primary-blue-500 font-caveat font-light">Hi, I&apos;m Vicky!</h2>
-      <h1 className="bg-gradient-to-r from-primary-dark-400 to-white bg-clip-text md:text-5xl text-3xl text-transparent md:h-14 inline-block font-bold">
+      <h1 className="bg-gradient-to-r from-primary-dark-400 to-white bg-clip-text text-transparent md:text-5xl text-3xl md:h-14 inline-block font-bold">
         Frontend Engineer
       </h1>
       <p className="text-primary-dark-500 md:text-base text-sm md:leading-8 leading-6">
