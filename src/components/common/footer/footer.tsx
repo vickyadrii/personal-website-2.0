ce2b117f69b29795992169598445691304b80dcf
vickyadri29 <vickyadri290501@gmail.com>
2025-01-07 14:18:41 +0700

    chore: update the method on footer component

--- src/components/common/footer/footer.tsx
+++ src/components/common/footer/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="pt-5">
       <div className="flex justify-between items-center">
         <p className="text-primary-dark-500 md:text-base text-sm flex items-center gap-1.5">
-          Vicky <span className="md:block hidden shrink-0">Herdiansyah Adri</span> &copy; {date.getFullYear()}
+          Vicky <span className="md:block hidden shrink-0">Herdiansyah Adri</span> &copy; {date.getUTCFullYear()}
         </p>
         <SocialMedia isResume={false} />
       </div>
